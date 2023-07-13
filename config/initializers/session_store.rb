86ef03df4d0476bf927b5b2f01849b9e99a1d757
romain isoz <isozromain@gmail.com>
2023-07-13 22:48:52 +0200

    added session_store.rb app name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification',
-                                                        domain: 'name-of-you-app-json-api'
+                                                        domain: 'https://app-back.jcloud.ik-server.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification'
 end
