0d5a9f971cf1b644264aa6c7b21b7be20d56f396
romain isoz <isozromain@gmail.com>
2023-07-13 22:58:53 +0200

    test to change domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification',
-                                                        domain: 'https://app-back.jcloud.ik-server.com/'
+                                                        domain: 'https://app-front.jcloud.ik-server.com/'
 else
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification'
 end
