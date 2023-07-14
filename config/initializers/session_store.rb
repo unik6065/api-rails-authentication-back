7630dcbab05402099afc155a99ca3bfcd2f7165d
romain isoz <isozromain@gmail.com>
2023-07-14 18:13:26 +0200

    try to improve cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,8 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification',
-                                                        domain: 'https://app-front.jcloud.ik-server.com/'
+                                                        domain: 'https://app-front.jcloud.ik-server.com/',
+                                                        httponly: true,
+                                                        same_site: :none
 else
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification'
 end
