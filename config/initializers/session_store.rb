f9427bb9a4ad7ccfc50b8548144efdde3f6c771b
romain isoz <isozromain@gmail.com>
2023-07-14 18:24:41 +0200

    change cookies domain to :all

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification',
-                                                        domain: 'https://app-front.jcloud.ik-server.com/',
+                                                        domain: :all,
                                                         httponly: true,
                                                         same_site: :none
 else
