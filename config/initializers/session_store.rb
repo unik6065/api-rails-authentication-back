56163d1b1c6799f8a19c09e65c3b97fb8af23245
romain isoz <isozromain@gmail.com>
2023-07-15 12:19:35 +0200

    changes some cookies settings

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,8 @@ if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification',
                                                         domain: :all,
                                                         httponly: true,
-                                                        same_site: :none
+                                                        same_site: :none,
+                                                        secure: true
 else
   Rails.application.config.session_store :cookie_store, key: '_api-rails-authentification'
 end
